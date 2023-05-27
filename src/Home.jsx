398d4b4302f627eff0ef35cbcd37b1214cefd8d4
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-27 17:17:19 -0400

    image updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,11 @@ export function Home() {
   return (
     <div>
       <h1 className="m-5">Welcome Baby</h1>
-      <img className="rounded-3" src="./src/assets/baby-oliver.png" alt="" />
+      <img
+        className="rounded-3"
+        src="https://previews.dropbox.com/p/thumb/AB7iQ9CzsKxCMg7FAGxd5b2qh0bQ2P5ZqkMuKguHoQwN4BlLALTLX3aHIFH09UnhVKuTnfTX5yA2gRSKSWpPLAVXXr7amBBAwMtF3t9frzecF_mS-p6XssBHc_zvMQKgVLYNehviEfELPl8r_I5OAUWQllOVqhJM6HdQ-uBC0oc3LqQbUZwsHOjbpxhLNlL50qeTQWtj_yiAQCwoJ5FlLNWzhiHWYVsCarO8Id8N877GP0wpe4mYlO1pMBkpyMpBE7jZyapHzXNH2Zc2q3KFzfdpTszQwFKPTcyCyZgndRS2QlY6ZGPhbk3R5zEmzt4UogLjk_mdw-PI9MnmkH2L4tLpb7964Ix-lJH-JRNGKtQ1RTWgfReob3gOPRLLHZDqZuE/p.png"
+        alt=""
+      />
     </div>
   );
 }
