f69fdb6a8d082dbc57c51aa0df8edb98d44f5423
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-27 16:47:35 -0400

    add home, header, footer, and content pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { Content } from "./Content";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
